7c6011e8a45fc3a1aae1c42f184ceca67708018d
juhwanlocal <jaiuer@hotmail.com>
2019-01-24 21:24:46 +0900

    6주차 2교시 코드입력.

--- app/src/main/java/com/example/mydatabase2/MainActivity.java
+++ app/src/main/java/com/example/mydatabase2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.mydatabase2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+    }
+
+    class SingerAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
